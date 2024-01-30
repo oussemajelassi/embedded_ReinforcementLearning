d231a7a698564e8a012768a2cdd81e703adb8ce2
oussemajelassi <oussjls57@gmail.com>
2024-01-30 19:57:24 +0100

    Header Files : structs, enums and macro like functions

--- TinyRL/CM7/Core/Inc/TinyRL.h
+++ TinyRL/CM7/Core/Inc/TinyRL.h
@@ -8,6 +8,25 @@
 #ifndef INC_TINYRL_H_
 #define INC_TINYRL_H_
 
+// Funtion Like Macro
+
+
+#define ROBOT_MOTOR_CLOCKWISE		\
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET ); \
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET ); \
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_SET ); \
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_RESET ) \
+
+
+#define ROBOT_MOTOR_COUNTERCLOCKWISE	\
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET ); \
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET ) ; \
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET ); \
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_SET ) \
+
+#define ROBOT_MOTOR_MODIFY_PWM	TIM1->CCR1 = ROBOT_MotorPWM ; \
+								TIM1->CCR2 = ROBOT_MotorPWM
+
 typedef struct
 {
 	uint32_t	TimeStamp;
@@ -23,5 +42,6 @@ typedef enum
 	ROBOT_UpdatingOrders,
 	ROBOT_GivingBackObservations
 }ROBOT_States;
+
 
 #endif /* INC_TINYRL_H_ */
